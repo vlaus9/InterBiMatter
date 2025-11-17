c118345e7dd79eb9d0ef1b7915f9353a8f1927a2
vlaus9 <poroxnenko_official@bk.ru>
2025-11-17 19:26:45 +0300

    feat/ref: Разделение структуры на фронт и бэк, настройка локальной БД

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -3,6 +3,7 @@ import type { Application, Request, Response } from "express"
 import mongoose from "mongoose"
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
 dotenv.config()
 
@@ -11,22 +12,49 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 
-const MONGODB_URL: string = process.env.MONGODB_URL || 'mongodb://localhost:27017/auth-app'
 
-mongoose.connect(MONGODB_URL)
-    .then(() => console.log('MongoDB connectes successfully'))
-    .catch((error: Error) => {
-        console.error('MongoDB connected error:', error)
-        process.exit(1)
-})
+// const MONGODB_URL: string = process.env.MONGODB_URL || 'mongodb://localhost:27017/InterBiMatterDB'
 
-app.use('/api/auth', require('./routes/auth').default)
+// mongoose.connect(MONGODB_URL)
+//     .then(() => console.log('MongoDB connectes successfully'))
+//     .catch((error: Error) => {
+//         console.error('MongoDB connected error:', error)
+//         // process.exit(1)
+// })
+
+
+let mongod: MongoMemoryServer
+
+const startServer = async() => {
+    try {
+
+        mongod = await MongoMemoryServer.create()
+        const MONGODB_URL = mongod.getUri()
+
+        await mongoose.connect(MONGODB_URL)
+        console.log('✅ MongoDB Memory Server connected successfully')
+        console.log(`📊 Database URL: ${MONGODB_URL}`)
+
+        const PORT: number = parseInt(process.env.PORT || '5000', 10)
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+            console.log(`🗄️  Database: MongoDB in Memory`)
+        })
+    }
+    catch (error) {
+        console.error('Server startup error:', error)
+    }
+}
+
+startServer()
+
+app.use('/api/auth', require('../middleware/routes/authRouter').default)
 
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Auth API is running'})
 })
 
-app.use('*', (req: Request, res: Response) => {
+app.use((req: Request, res: Response) => {
     res.status(404).json({ message: 'Route not found' })
 })
 
---
+++ frontend/src/components/Profile/Profile.tsx
@@ -0,0 +1,40 @@
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { logOut } from "../Auth/slices/authSlice"
+
+const Profile: React.FC = () => {
+    const { user } = useAppSelector((state) => state.authSlice)
+    const dispatch = useAppDispatch()
+
+    const handleLogOut = () => {
+        dispatch(logOut())
+    }
+
+    return (
+        <div className={'w-[600px]'}>
+            <h2>Профиль</h2>
+            {user ? (
+                <div className={'border p-[20px] rounded-[8px] bg-[#f9f9f9]'}>
+                    <div>
+                        <strong>
+                            { user.email }
+                        </strong>
+                    </div>
+                    <div>
+                        <strong>
+                            { user.id }
+                        </strong>
+                    </div>
+                    <button onClick={handleLogOut}>
+                        Выйти
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <p>Пожалуйста, зарегистрируйтесь...</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Profile
