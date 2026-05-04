82943ad82b5a7b9f612c7b91f6e6fc7dfee52d6b
Владислав Порохненко <vladislavporohnenko@MacBook-Pro-Vladislav.local>
2026-05-04 23:18:32 +0300

    feat:Продолжаю делать компонент добавления файла в проект

--- frontend/src/components/AddFileToProjectForm/AddFileToProjectForm.tsx
+++ frontend/src/components/AddFileToProjectForm/AddFileToProjectForm.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 const AddFileToProjectForm: React.FC = () => {
     
     const [file, setFile] = useState<File>()
+    const [version, setVersion] = useState<string>('')
+    const [description, setDescription] = useState<string>('')
     
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files.length > 0) {
@@ -21,7 +23,7 @@ const AddFileToProjectForm: React.FC = () => {
             variants={AnimateVariants.createProjectFormVariant}
             className='absolute flex justify-center items-center w-full h-full bg-black/30 backdrop-blur-xl'>
             
-            <div className='w-[1200px] min-w-[500px] h-[1000px] min-h-[600px] bg-(--bg-primary) border-2 border-(--button-group-primary-bg) m-[30px] rounded-[15px]'>
+            <div className='w-[60%] min-w-[500px] h-[70%] min-h-[600px] bg-(--bg-primary) border-2 border-(--button-group-primary-bg) m-[30px] rounded-[15px]'>
                 <div className='flex justify-center items-center h-20'>
                     <h1 className='text-(--text-primary) text-[26px] mb-2.5' style={{fontWeight: '800'}}>
                         Добавить файл в проект
@@ -29,9 +31,17 @@ const AddFileToProjectForm: React.FC = () => {
                 </div>
 
                 <form className='h-[80%] flex flex-col justify-center items-center'>
-                    <div className='flex gap-3 w-[800px] justify-between'>
-                        <h3 className='text-[18px] text-(--text-primary)'>Версия файла</h3>
-                        <label className='flex justify-center items-center cursor-pointer px-[15px] ml-[50px] w-[300px] h-[45px] bg-(--button-group-primary-bg) rounded-sm text-[18px] border text-(--text-primary) hover:border-[#d0cfcfff] hover:border-2 transition-all'>Выбрать файл модели</label>
+                    <div className='flex gap-3 w-[80%] justify-between mb-10'>
+                        <label htmlFor='fileVersion' className='text-[18px] text-(--text-primary) pt-1.5'>Версия файла</label>
+                        <input id='fileVersion' type='text' placeholder='Например:  V.0.1' value={version} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVersion(e.target.value)} required className='w-[600px] h-[45px] p-2 border border-black bg-(--button-group-primary-bg) outline-(--button-group-primary-bg) rounded-sm text-(--text-primary) placeholder:text-[black] placeholder:opacity-[0.5]'></input>
+                    </div>
+                    <div className='flex gap-3 w-[80%] justify-between mb-10'>
+                        <label htmlFor='fileVersion' className='text-[18px] text-(--text-primary) pt-1.5'>Описание</label>
+                        <textarea id='fileVersion' type='text' placeholder='Опишите внесенные в файл изменения...' value={description} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDescription(e.target.value)} required className='w-[600px] h-[135px] h-max-[300px] p-2 border border-black bg-(--button-group-primary-bg) outline-(--button-group-primary-bg) rounded-sm text-(--text-primary) placeholder:text-[black] placeholder:opacity-[0.5]'></textarea>
+                    </div>
+                    <div className='flex gap-3 w-[80%] justify-between'>
+                        <h3 className='text-[18px] text-(--text-primary) pt-1.5'>Файл модели:</h3>
+                        <label htmlFor='fileProject' className='flex justify-center items-center cursor-pointer px-[15px] ml-[50px] w-[600px] h-[45px] bg-(--button-group-primary-bg) rounded-sm text-[18px] border text-(--text-primary) hover:border-[#d0cfcfff] hover:border-2 transition-all'>Выбрать файл модели</label>
                         <input id='fileProject' type='file' className='hidden' accept='.ifc' onChange={handleFileChange}></input>
                     </div>
                     
--- frontend/src/components/CreateProjectForm/CreateProjectForm.tsx
+++ frontend/src/components/CreateProjectForm/CreateProjectForm.tsx
@@ -75,32 +75,32 @@ const CreateProjectForm: React.FC = () => {
         exit={AnimateVariants.createProjectFormVariant.out}
         variants={{AnimateVariants}}
         className='absolute flex justify-center items-center w-full h-full bg-black/30 backdrop-blur-xl'>
-            <div className='w-[800px] min-w-[400px] h-[600px] min-h-[200px] bg-[var(--bg-primary)] border-[2px] border-[var(--button-group-primary-bg)] m-[30px] rounded-[15px]'>
-                <div className='flex justify-center items-center h-[80px]'>
-                    <h1 className='text-[var(--text-primary)] text-[26px] mb-[10px]' style={{ fontWeight: 800 }}>Создание проекта</h1>
+            <div className='w-[800px] min-w-[400px] h-[600px] min-h-[200px] bg-(--bg-primary) border-2 border-(--button-group-primary-bg) m-[30px] rounded-[15px]'>
+                <div className='flex justify-center items-center h-20'>
+                    <h1 className='text-(--text-primary) text-[26px] mb-2.5' style={{ fontWeight: 800 }}>Создание проекта</h1>
                 </div>
                 <form onSubmit={handleSubmit} className='h-[80%] flex flex-col justify-center items-center'>
-                    <div className='mb-[20px] w-[600px] flex justify-between items-center'>
-                        <label htmlFor='nameProject' className='mb-[10px] mt-[10px] text-[18px] text-[var(--text-primary)]'>Название проекта</label>
-                        <input id='nameProject' type='text' placeholder='Введите название проекта...' value={nameProject} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNameProject(e.target.value)} required className='w-[300px] h-[45px] p-[8px] border border-[black] bg-[var(--button-group-primary-bg)] outline-[var(--button-group-primary-bg)] rounded-[4px] text-[var(--text-primary)] placeholder:text-center placeholder:text-[black] placeholder:opacity-[0.5]'></input>
+                    <div className='mb-5 w-[600px] flex justify-between items-center'>
+                        <label htmlFor='nameProject' className='mb-2.5 mt-2.5 text-[18px] text-(--text-primary)'>Название проекта</label>
+                        <input id='nameProject' type='text' placeholder='Введите название проекта...' value={nameProject} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNameProject(e.target.value)} required className='w-[300px] h-[45px] p-2 border border-[black] bg-(--button-group-primary-bg) outline-(--button-group-primary-bg) rounded-sm text-(--text-primary) placeholder:text-center placeholder:text-[black] placeholder:opacity-[0.5]'></input>
                     </div>
-                    <div className='mb-[20px] w-[600px] flex justify-between items-center'>
-                        <label htmlFor='nameProject' className='mb-[10px] mt-[10px] text-[18px] text-[var(--text-primary)]'>Версия файла модели</label>
-                        <input id='nameProject' type='text' placeholder='Введите название версии...' value={version} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVersion(e.target.value)} required className='w-[300px] h-[45px] p-[8px] border border-[black] bg-[var(--button-group-primary-bg)] outline-[var(--button-group-primary-bg)] rounded-[4px] text-[var(--text-primary)] placeholder:text-center placeholder:text-[black] placeholder:opacity-[0.5]'></input>
+                    <div className='mb-5 w-[600px] flex justify-between items-center'>
+                        <label htmlFor='nameProject' className='mb-2.5 mt-2.5 text-[18px] text-(--text-primary)'>Версия файла модели</label>
+                        <input id='nameProject' type='text' placeholder='Введите название версии...' value={version} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVersion(e.target.value)} required className='w-[300px] h-[45px] p-[8px] border border-[black] bg-(--button-group-primary-bg) outline-(--button-group-primary-bg) rounded-sm text-(--text-primary) placeholder:text-center placeholder:text-[black] placeholder:opacity-[0.5]'></input>
                     </div>
                     <div className='flex justify-between items-center w-[600px] '>
-                        <h3 className='text-[18px] text-[var(--text-primary)]'>Файл модели</h3>
+                        <h3 className='text-[18px] text-(--text-primary)'>Файл модели</h3>
 
                         {selectedFiles.length === 0 ?
                             <>
-                                <label htmlFor="fileProject" className='flex rounded-[4px] px-[15px] ml-[50px] w-[300px] h-[45px] bg-[var(--button-group-primary-bg)] justify-center items-center border cursor-pointer hover:border-[#d0cfcfff] hover:border-[2px] transition-all'>
+                                <label htmlFor="fileProject" className='flex rounded-sm px-[15px] ml-[50px] w-[300px] h-[45px] bg-(--button-group-primary-bg) justify-center items-center border cursor-pointer hover:border-[#d0cfcfff] hover:border-2 transition-all'>
                                 Загрузить файл модели
                                 </label>
                                 <input id='fileProject' type='file' accept='.ifc' className='hidden' onChange={handleFileChange} multiple></input>
                             </>
                         :
-                            <div className='flex flex-col justify-center gap-[10px]'>
-                                <label htmlFor="fileProject" className='flex rounded-[4px] px-[15px] w-[300px] h-[45px] bg-[var(--button-group-primary-bg)] justify-center items-center border cursor-pointer hover:border-[#d0cfcfff] hover:border-[2px] transition-all'>
+                            <div className='flex flex-col justify-center gap-2.5'>
+                                <label htmlFor="fileProject" className='flex rounded-sm px-[15px] w-[300px] h-[45px] bg-[var(--button-group-primary-bg)] justify-center items-center border cursor-pointer hover:border-[#d0cfcfff] hover:border-[2px] transition-all'>
                                 Выбрать другой файл
                                 </label>
                                 <input id='fileProject' type='file' accept='.gltf, .glb, .bin, .jpf, .jpeg, .png, .mtl, .ifc' className='hidden' onChange={handleFileChange} multiple></input>
